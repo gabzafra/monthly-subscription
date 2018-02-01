65e54ac34fce892778388d65af7eea6b17745ccb
gabzafra <gab.zafra@gmail.com>
2018-02-01 16:10:43 +0100

    Initial commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    $title = $faker->realText(20);
+    $slug  = srt_slug($title, '-');
+    return [
+        'user_id' => function() { return factory(App\User::class)->create()->id},
+        'title'   => $title,
+        'slug'    => $slug,
+        'image'   => $faker->imageUrl(1200, 600, 'animals'),
+        'content' => $faker->paragraphs(10 , true),
+        'premium' => rand(0, 1)
+    ];
+});
